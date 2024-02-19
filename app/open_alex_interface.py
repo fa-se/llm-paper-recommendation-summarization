ce3493f11777db27ed377a8a74580d3178b19ada
fa-se <seuring@campus.tu-berlin.de>
2024-02-19 13:10:31 +0100

    Added relevance_score to Work, as returned by OpenAlex search queries. https://docs.openalex.org/how-to-use-the-api/get-lists-of-entities/search-entities#relevance-score
    Signed-off-by: Fabian Seuring <seuring@campus.tu-berlin.de>

--- app/open_alex_interface.py
+++ app/open_alex_interface.py
@@ -11,9 +11,11 @@ class Work:
         # First three authors for now
         self.authors = [author['author']['display_name'] for author in pyalex_work['authorships'][:3]]
         self.id = pyalex_work['ids']['openalex']
+        # Depending on the query, pyalex may not return a relevance score
+        self.relevance_score = pyalex_work.get('relevance_score', None)
 
     def __str__(self):
-        return f"'{self.title}' by [{', '.join(self.authors)}] ({self.id})"
+        return f"'{self.title}' by [{', '.join(self.authors)}] ({self.id}, relevance score: {self.relevance_score})"
 
 
 def search_works(query: str) -> [Work]:
