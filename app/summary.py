c9d4cb4d82dd0a55eb15b7ef43d40377633a78f8
fa-se <seuring@campus.tu-berlin.de>
2024-02-21 18:16:39 +0100

    Added/fixed return type annotations.
    Signed-off-by: Fabian Seuring <seuring@campus.tu-berlin.de>

--- app/summary.py
+++ app/summary.py
@@ -1,6 +1,7 @@
 from open_alex_interface import Work
 
 
-def summarize_work(work: Work) -> float:
+
+def summarize_work(work: Work) -> str:
     # return OpenAlex's relevance score for now
     return work.abstract
