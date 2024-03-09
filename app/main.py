66b23952938d478445a9ee3029a26acbfb83afe6
fa-se <seuring@campus.tu-berlin.de>
2024-03-09 23:56:09 +0100

    Added example function to main that utilizes both DB models and the LLMInterface to create embeddings for a user query and then find the 5 nearest topics using cosine similarity.

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 import typer
 
+from db.database import Session
+from llm_interfaces import LLMInterface, OpenAIInterface
 from open_alex_interface import Work, search_works
 from relevance import rate_relevance
 from summary import summarize_work
@@ -37,6 +39,36 @@ def search_rate_summarize(query: str, limit: int = typer.Option(3, help="Summari
     summarized_works = summarize(rated_works[:limit], query)
     for work in summarized_works:
         print(f"{work}\nSummary: {work.summary}\n")
+
+
+@app.command()
+def get_matching_topics(
+        ctx: typer.Context,
+        research_area_description: str = typer.Argument(..., help="A string describing the user's area of research.")
+):
+    llm_interface: LLMInterface = ctx.obj
+    query_embedding = llm_interface.create_embedding(research_area_description)
+
+    from sqlalchemy import select, desc
+    from db.models import Topic
+
+    with Session() as session:
+        statement = select(Topic, (1 - Topic.embedding.cosine_distance(query_embedding)).label(
+            'cosine_similarity')).order_by(desc('cosine_similarity')).limit(5)
+        results = session.execute(statement).fetchall()
+
+    for result in results:
+        typer.echo(f"{result.Topic.name}: {result.cosine_similarity}")
+
+
+@app.callback(invoke_without_command=True)
+def main(ctx: typer.Context):
+    if ctx.invoked_subcommand is None:
+        typer.echo("Please specify a subcommand.")
+        raise typer.Exit(1)
+    # instantiate LLMInterface, and add it to context so that it's available in command handlers
+    llm_interface: LLMInterface = OpenAIInterface()
+    ctx.obj = llm_interface  # TODO: define custom class for context or use ctx.params
 
 
 if __name__ == "__main__":
