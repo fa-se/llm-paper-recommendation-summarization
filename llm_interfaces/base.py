65c1f893ea176a341ec81b7045e99a275b58199f
fa-se <seuring@campus.tu-berlin.de>
2024-03-13 13:59:31 +0100

    Added the means to generically define text completion Tasks, which can be handled by LLMInterfaces. A task provides the get_prompt method, which returns a list of Messages to be used as input to an LLM. Messages consist of role (e.g. "user", "system"). Concrete Task implementations can define message templates which can be filled with instance specific user data in get_prompt. Utilizing the LLMType passed to get_prompt, implementations can use different templates for different LLMs.

--- llm_interfaces/base.py
+++ llm_interfaces/base.py
@@ -1,3 +1,62 @@
+from enum import Enum, auto
+from typing import Self
+
+
+class LLMType(Enum):
+    GPT = auto()
+
+
+class Message:
+    roles = ["system", "user", "assistant"]
+
+    def __init__(self, role: str, content: str):
+        if role not in self.roles:
+            raise ValueError(f"Invalid role: {role}")
+        self.role = role
+        self.content = content
+
+    def format(self, **kwargs) -> Self:
+        """
+        This function returns a formatted copy of the Message instance.
+
+        Parameters:
+            **kwargs: The keyword arguments to pass to str.format.
+
+        Returns:
+            Message
+        """
+        return Message(self.role, self.content.format(**kwargs))
+
+
+class Task:
+    """
+    Base class that encapsulates a task to be handled by an LLM interface.
+    Inheriting classes can define custom logic to generate the corresponding prompt, e.g. to adapt the prompt to different LLMs.
+    """
+
+    def __init__(self, prioritize_quality: bool = False):
+        """
+        Parameters:
+            prioritize_quality (bool): Indicates whether to prioritize quality over cost for this task, e.g. by using larger models.
+        """
+        self.prioritize_quality = prioritize_quality
+
+    def get_prompt(self, llm_type: LLMType) -> [Message]:
+        """
+        Generate the prompt for the specified LLM type.
+
+        Parameters:
+            llm_type: The LLM type for which to get the prompt.
+
+        Returns:
+            [Message]: The messages representing the prompt for the specified LLM type.
+        """
+        raise NotImplementedError
+
+
 class LLMInterface:
     def create_embedding(self, text: str, config: dict = None) -> list[float]:
+        raise NotImplementedError
+
+    def handle_task(self, task: Task) -> str:
         raise NotImplementedError
--- llm_interfaces/openai.py
+++ llm_interfaces/openai.py
@@ -1,25 +1,39 @@
 from os import environ
 
 from openai import OpenAI
+from openai.types.chat import ChatCompletionMessageParam, ChatCompletionSystemMessageParam, \
+    ChatCompletionUserMessageParam
 
-from .base import LLMInterface
+from .base import LLMInterface, LLMType, Message, Task
 
 
 class OpenAIInterface(LLMInterface):
     defaults = {
+        # https://platform.openai.com/docs/models/gpt-4-and-gpt-4-turbo
+        "quality_model": "gpt-4-0125-preview",
+        "budget_model": "gpt-3.5-turbo-0125",
         "embedding_model": "text-embedding-3-large",
         "embedding_dimensions": 1024
     }
 
     model_to_cost_per_token = {
+        # https://openai.com/pricing
+        "gpt-4-0125-preview": 10.00 / 1e6,
+        "gpt-3.5-turbo-0125": 0.50 / 1e6,
         "text-embedding-3-large": 0.13 / 1e6
-        # $0.13 per 1,000,000 tokens, as of March 2024 (https://openai.com/pricing)
     }
 
     def __init__(self, print_usage_info: bool = True):
         self.client = OpenAI(api_key=environ.get("OPENAI_API_KEY"))
         self.accumulated_costs = 0.0
         self.print_usage_info = print_usage_info
+
+    def handle_task(self, task: Task) -> str:
+        messages = task.get_prompt(LLMType.GPT)
+        model = self.defaults["quality_model"] if task.prioritize_quality else self.defaults["budget_model"]
+        completion = self.create_completion(messages=messages, model=model)
+
+        return completion
 
     def create_embedding(self, text: str, config: dict = None) -> list[float]:
         if config is None:
@@ -38,3 +52,26 @@ class OpenAIInterface(LLMInterface):
                 f"Model: {config['embedding_model']}, Tokens: {used_tokens}, Cost: ${cost:.2f}, Accumulated cost: ${self.accumulated_costs:.2f}")
 
         return response.data[0].embedding
+
+    def create_completion(self, messages: [Message], model: str) -> str:
+        completion_messages: [ChatCompletionMessageParam] = []
+        for message in messages:
+            # Due to ChatCompletionMessageParam being a union, we need to check the role and instantiate the correct type
+            if message.role == 'system':
+                message_param = ChatCompletionSystemMessageParam(role=message.role, content=message.content)
+            elif message.role == 'user':
+                message_param = ChatCompletionUserMessageParam(role=message.role, content=message.content)
+            else:
+                raise ValueError(f"Unsupported message role: {message.role}")
+            completion_messages.append(message_param)
+
+        response = self.client.chat.completions.create(messages=completion_messages, model=model)
+
+        used_tokens = response.usage.total_tokens
+        cost = used_tokens * self.model_to_cost_per_token[model]
+        self.accumulated_costs += cost
+        if self.print_usage_info:
+            print(
+                f"Model: {model}, Tokens: {used_tokens}, Cost: ${cost:.2f}, Accumulated cost: ${self.accumulated_costs:.2f}")
+
+        return response.choices[0].message.content.strip()
